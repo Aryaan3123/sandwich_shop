951285bb1db26dc553c9e47801b372fc386ab462
Aryaan3123 <aryaandeeba@gmail.com>
2025-11-14 12:21:56 +0000

    Test inital commit

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'Sandwich Shop App',
+      title: 'Sandwich Shop App (Week 4)',
       home: OrderScreen(maxQuantity: 5),
     );
   }
